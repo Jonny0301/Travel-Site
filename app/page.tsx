37071f2e531ec568fd93d404d93bfaaf56686eb7
Jonny0301 <stevenperdomo2424@gmail.com>
2024-08-07 20:24:23 -0700

    pdate

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
       <Hero />
       <AboutUs />
       <HowWeWork />
-      <PricingCards />
       <Gallery />
+      <PricingCards />
       <Contact />
     </>
   );
